091b924137ba94d59ee163b824d91284a0ec6c91
quantumsheep <nathanael.dmc@outlook.fr>
2021-12-07 00:05:03 +0100

    Disable some ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
     'prettier/prettier': [
       'error',
       {
